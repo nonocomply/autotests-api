6b3d10bd9484b7743ceb65f47549be452b63894f
nonocomply <hvickismywaifu@gmail.com>
2025-06-17 23:39:00 +0300

    Добавляем информацию о переменных окружения в Allure отчет. Практика работы с переменным окружения в Allure

---
+++ fixtures/allure.py
@@ -0,0 +1,13 @@
+import pytest
+
+from tools.allure.environment import create_allure_environment_file
+
+
+@pytest.fixture(scope="session", autouse=True)
+def save_allure_environment_file():
+    yield
+    create_allure_environment_file()
+
+
+if __name__ == '__main__':
+    pass
