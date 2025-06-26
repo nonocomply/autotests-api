4e228318af087b8f8bc10900d18f18de7153bf19
nonocomply <hvickismywaifu@gmail.com>
2025-06-26 23:35:05 +0300

    added autotest run on CI

--- config.py
+++ config.py
@@ -19,7 +19,10 @@ class TestDataConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", env_nested_delimiter="."
+        extra="allow",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter=".",
     )
 
     test_data: TestDataConfig
